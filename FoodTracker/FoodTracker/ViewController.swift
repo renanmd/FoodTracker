71ebd5d617a0a3cd94b9108912d15793568bd2da
Renan Muller <renanmd@gmail.com>
2020-02-23 15:55:50 -0300

    Commiting first changes to repo

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Renan Diniz on 23/02/20.
+//  Copyright © 2020 RMD Goup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
