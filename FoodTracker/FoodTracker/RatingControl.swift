d75dd9f37546dc996ab1ee62c4320ab3cf8df124
Renan Muller <renanmd@gmail.com>
2020-02-23 21:02:57 -0300

    Updating the repo

---
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Renan Diniz on 23/02/20.
+//  Copyright © 2020 RMD Goup. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
